8f56162c371284bf21517b3af964063950b3cbda
Mahigavale <maheshgavale07@gmail.com>
2025-09-29 18:20:53 +0530

    ABS

--- oops/src/abstraction/First.java
+++ oops/src/abstraction/First.java
@@ -2,7 +2,7 @@ package abstraction;
 
 public class First {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		/**
 		 * Abstract=> Incomplete.
@@ -10,9 +10,49 @@ public class First {
 		 * 1)Abstract class.
 		 * 2)An Interface.
 		 */
-/*
- * 
- */
+		
+		
+              Company com=Company.getObj();
 	}
+}
 
+class Company
+{
+	int id;
+	String name;
+	
+	public void setId(int id)
+	{
+		this.id=id;
+	}
+	
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	
+	public int getId()
+	{
+		return this.id;
+	}
+	
+	public String getName()
+	{
+		
+		
+		return this.name;
+	}
+	
+	
+	public static Company getObj() throws InterruptedException
+	{
+		
+		
+		return new Company();
+	}
+	
+	public Company()
+	{
+		System.out.println("Object created.....");
+	}
 }
