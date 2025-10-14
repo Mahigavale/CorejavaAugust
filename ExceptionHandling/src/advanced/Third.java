03b93e7120e2e8c785e413f3e769409307bc6c58
Mahigavale <maheshgavale07@gmail.com>
2025-10-14 18:23:24 +0530

    Exception handling

--- ExceptionHandling/src/advanced/Third.java
+++ ExceptionHandling/src/advanced/Third.java
@@ -5,26 +5,12 @@ public class Third {
 	public static void main(String[] args) {
 	
 		
-		try
-		{
+		
 		if(10>5 && 40>30)
 		{
 			throw  new invalidDayException("Chukicha Diwas!");
 		}
 		
-		else if(100>90 && 70 >68)
-		{
-			 throw new Exception("demooooooo");
-		}
-		}
-		catch(invalidDayException ex)
-		{ System.out.println(1);
-			System.out.println(ex.getMessage());
-		}
-		catch(Exception ex)
-		{
-			System.out.println(ex.getMessage());
-		}
 		
 		System.out.println("THE END");
 	}
