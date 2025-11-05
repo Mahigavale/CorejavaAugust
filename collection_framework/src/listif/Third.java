d46a90b606a6f9f45ccf08bf131576cbfea341f6
Mahigavale <maheshgavale07@gmail.com>
2025-11-05 15:50:54 +0530

    Core Java

---
+++ collection_framework/src/listif/Third.java
@@ -0,0 +1,17 @@
+package listif;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Third {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list=new ArrayList<Integer>();
+		
+		list.add(12);
+		list.add(23);
+		list.add("12");
+	}
+
+}
