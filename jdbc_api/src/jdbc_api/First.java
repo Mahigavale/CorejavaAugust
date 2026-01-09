aacc3306c3f2ea0ceb69368deaa56ea43a278fc1
Mahigavale <maheshgavale07@gmail.com>
2026-01-09 15:48:04 +0530

    JDBC

--- jdbc_api/src/jdbc_api/First.java
+++ jdbc_api/src/jdbc_api/First.java
@@ -6,14 +6,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class First {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		// Class.forName("com.mysql.cj.jdbc.Driver");
+		 Class.forName("com.mysql.cj.jdbc.Driver");
 
+		 Scanner scan=new Scanner(System.in);
 		// step=> opening a connection.jdbc2
 
+		 //PrepareStateMent
+		 int id;
+		 System.out.println("Please enter the id:");
+		 id=scan.nextInt();
+		 System.out.println("please enter the name:");
+		 String name=scan.next();
 		Properties prop = new Properties();
 		prop.put("user", "root");
 		prop.put("password", "root");
@@ -21,7 +29,7 @@ public class First {
 				Statement stmt = con.createStatement();) {
 			System.out.println("connection opened!");
 
-			int n = stmt.executeUpdate("insert into Student values(2,'def'),(3,'ghi');");
+			int n = stmt.executeUpdate("insert into Student values()");
 
 			System.out.println("no of rows affected..."+n);
 			Integer i1=128;
